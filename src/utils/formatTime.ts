53c5c2791359972e78366b6f3b9f1898c2310f32
matt <wootae.jung@beautyselection.co.kr>
2023-11-28 14:50:16 +0900

    v1.0.0 리뷰합니다. review를 검색해주세요

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,3 +1,6 @@
+// review
+// 함수이름에 의도가 명확하게 보이면 좋을 것 같아요
+// toYYMMDDSplitedByDot 은 어떠신가요?
 export const formatTime = (timeString: string): string | null => {
   const date = new Date(timeString)
 
